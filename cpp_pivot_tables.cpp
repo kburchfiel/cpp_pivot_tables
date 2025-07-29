7f0b0bf23688b1f151eef54f1a29ee72ea93e93f
Kenneth Burchfiel <kburchfiel@gmail.com>
2025-07-29 00:05:18 -0400

    Updated cpp_pivot_tables.cpp

--- cpp_pivot_tables.cpp
+++ cpp_pivot_tables.cpp
@@ -3,6 +3,11 @@
 By Kenneth Burchfiel
 Released under the MIT License
 
+(Note: I am grateful to Carter Skeel and Wendy Wang at the 
+Institute for Family Studies for their openness to my making
+this project open source. This code was written in my free time
+but incorporates ideas from my work.)
+
 This project shows how to create simple pivot tables within C++
 via a custom function.
 
@@ -14,6 +19,9 @@ https://github.com/vincentlaucsb/csv-parser
 https://vincela.com/csv/classcsv_1_1CSVRow.html 
 
 # To dos:
+
+1. Consider using tuples rather than strings for your index values
+(as you haven't tried out that approach yet.)
 
 1. Show how pivot tables can be created via data already stored in
 memory as vectors of classes (which will be more realistic than,
@@ -53,13 +61,16 @@ fu146_anzr=Nv4+Pn44vr45
 #include <string>
 #include <map>
 #include <numeric> // for std::accumulate
+#include <chrono>
+#include <functional>
 
 using namespace csv;
 
 
 void scan_to_pivot(std::string& data_file_path, 
-std::vector<std::string>& pivot_index, std::string& pivot_val,
-long& rows_to_scan, std::string& pivot_file_path)
+std::string& pivot_val, std::string index_headers,
+long& rows_to_scan, std::string& pivot_file_path,
+std::function<std::string(CSVRow)> index_gen)
 {
 /*This function creates a pivot table by scanning through a 
 .csv file (rather than importing all of it into your RAM).
@@ -69,6 +80,13 @@ the final table.
 rows_to_scan: A long integer that allows you to scan in only
 a subset of rows (which can be helpful for debugging/testing 
 purposes). To scan all rows, set rows_to_scan to -1.
+
+
+The value of index_gen() will be defined via a lambda function
+whenever scan_to_pivot gets called. For more on the use of
+std::function, see
+https://www.geeksforgeeks.org/cpp/passing-a-function-as-
+a-parameter-in-cpp/ .
 
 (More documentation to come)
 */
@@ -98,9 +116,9 @@ purposes). To scan all rows, set rows_to_scan to -1.
     // pivot index combination:
     // Note that each value will be initialized as 0.0 (or, for 
     // long integers, 0) in order
-    // to help ensure that our final output is correct. (Not sure
-    // whether this is strictly necessary to specify, but it 
-    // doesn't hurt.)
+    // to help ensure that our final output is correct. (I'm not sure
+    // that these initial values are strictly necessary to specify, 
+    // but it doesn't hurt to do so.)
 
     struct Pivot_Vals
     {
@@ -109,7 +127,7 @@ purposes). To scan all rows, set rows_to_scan to -1.
         double pivot_mean {0.0};
     };
 
-    std::map<std::vector<std::string>, Pivot_Vals> pivot_map;
+    std::unordered_map<std::string, Pivot_Vals> pivot_map;
     //  {0.0, 0.0}
 
     long scanned_rows = 0;
@@ -117,12 +135,7 @@ purposes). To scan all rows, set rows_to_scan to -1.
     {
 
         if ((scanned_rows < rows_to_scan) || (rows_to_scan == -1))
-        {
-            std::vector<std::string> pivot_index_vals{};
-            for (std::string &variable : pivot_index)
-            {
-                pivot_index_vals.push_back(row[variable].get());
-            }
+        {   std::string pivot_index_vals = index_gen(row);
             // Adding the values corresponding to this set
             // of index variables to our map:
             // (If an entry for this index variable set
@@ -159,7 +172,7 @@ purposes). To scan all rows, set rows_to_scan to -1.
 
     // Writing a header row for the .csv file:
 
-    std::vector<std::string> header_row = pivot_index;
+    std::vector<std::string> header_row {index_headers};
     // Adding pivot value fields to this row:
     std::vector<std::string> value_fields{"Sum",
                                           "Count", "Mean"};
@@ -186,7 +199,7 @@ purposes). To scan all rows, set rows_to_scan to -1.
         // Initializing a vector of strings that will store the
         // data for the current key/value pair as a copy of
         // the pivot_index data:
-        std::vector<std::string> pivot_row_vector = pivot_index;
+        std::vector<std::string> pivot_row_vector {pivot_index};
         // Adding results (in string form) to this vector:
 
         pivot_row_vector.push_back(
@@ -208,29 +221,34 @@ purposes). To scan all rows, set rows_to_scan to -1.
 }
 
 int main()
-{
+{auto program_start_time = std::chrono::high_resolution_clock::now();
 std::string data_file_path {"/home/kjb3/D1V1/Documents/\
 Large_Datasets/BTS/T_T100_SEGMENT_ALL_CARRIER_2024.csv"};
-std::vector<std::string> pivot_index {"CARRIER", "ORIGIN", "REGION"};
+std::string index_headers {"CARRIER|ORIGIN|REGION"};
 std::string pivot_val {"DISTANCE"};
-long rows_to_scan {-1};
+long rows_to_scan {-1}; // Use -1 to scan all rows
 std::string pivot_file_path {
 "../mean_dist_by_carrier_origin_region.csv"};
 
 // Calling scan_to_pivot to calculate average distance values
 // by carrier, origin, and region:
-scan_to_pivot(data_file_path, pivot_index, pivot_val,
-rows_to_scan, pivot_file_path);
+scan_to_pivot(data_file_path, pivot_val, index_headers,
+rows_to_scan, pivot_file_path, [&](
+    CSVRow row) -> std::string {
+    return {row["CARRIER"].get() + "|" + row["ORIGIN"].get() + "|" + 
+    row["REGION"].get()};});
 
-// Updating several varialbes, then calling scan_to_pivot a 
+// Updating several variables, then calling scan_to_pivot a 
 // second time to calculate mean distance values by 
 // carrier and origin only:
 
 pivot_file_path = "../mean_dist_by_carrier_origin.csv";
-pivot_index = {"CARRIER", "ORIGIN"};
+index_headers = "CARRIER|ORIGIN";
 
-scan_to_pivot(data_file_path, pivot_index, pivot_val,
-rows_to_scan, pivot_file_path);
+scan_to_pivot(data_file_path, pivot_val, index_headers,
+rows_to_scan, pivot_file_path, [&](
+    CSVRow row) -> std::string {
+    return {row["CARRIER"].get() + "|" + row["ORIGIN"].get()};});
 
 // The following code is useful for checking the program's
 // RAM usage after all other code has been executed.
@@ -238,4 +256,26 @@ rows_to_scan, pivot_file_path);
 // char key;
 // std::cin >> key;
 
+auto program_end_time = std::chrono::high_resolution_clock::now();
+auto program_run_time = std::chrono::duration<double>(
+    program_end_time - program_start_time).count();
+std::cout << "The program finished running after "
+<< program_run_time << " seconds.\n";
 }
+
+// Runtime calcs:
+// (These all use the original, non-truncated versions of the 
+// datasets.)
+// Original approach (prior to implementing lambda function for
+// developing pivot index): 7.44317 seconds (1st run); 7.32014
+// seconds (second run); 7.24371 seconds (3rd run).
+
+// New approach (that uses lambda function for creating vector-
+// based pivot index: 7.02943 seconds (1st run);
+// 7.06246 seconds (2nd run); 7.09754 seconds (3rd run)
+
+// New approach (that uses strings rather than vectors to store
+// data): 5.91007 seconds; 5.88203 seconds; and 5.97761 seconds.
+
+// New approach (that uses unordered rather than ordered maps):
+// 5.87901 seconds; 5.92743 seconds; and 5.84311 seconds.
