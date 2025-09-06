92ece43fb82c640b38f785bee54430cce7f42ab5
Kenneth Burchfiel <kburchfiel@gmail.com>
2025-09-06 02:31:04 -0400

    Pivot table function can now produce aggregate stats for an arbitrary number of pivot values; formerly, only one value could be analyzed per function call. Also added some unflitered output examples

--- cpp_pivot_tables.cpp
+++ cpp_pivot_tables.cpp
@@ -29,14 +29,6 @@ say, vectors of maps in which every value has the same type).
 
 2. Add in more documentation.
 
-3. Add in timing-related code.
-
-4. Consider making the examples more interesting/relevant.
-(For instance, consider adding in code that will allow you to
-show passenger-mile or passenger totals rather than just
-distance totals. In addition, consider multiplying distance
-by departures so that you arrive at a more accurate/relevant
-distance total.)
 
 5. Download data from other years also so that you end up with
 a very large file; that will make it easier to determine how
@@ -57,12 +49,12 @@ fu146_anzr=Nv4+Pn44vr45
 */
 
 #include "csv.hpp"
+#include "pivot_compressors.h"
 #include <chrono>
 #include <functional>
 #include <iostream>
 #include <map>
 #include <string>
-#include "pivot_compressors.h"
 
 using namespace csv;
 
@@ -71,56 +63,79 @@ int main() {
   std::string data_file_path{"/home/kjb3/D1V1/Documents/\
 Large_Datasets/BTS/T_T100_SEGMENT_ALL_CARRIER_2024.csv"};
   std::string index_headers{"CARRIER|ORIGIN|REGION"};
-  std::string pivot_val{"PASSENGERS"};
-  long rows_to_scan {-1}; // Use -1 to scan all rows
-  std::string pivot_file_path{"../mean_passengers_by_carrier_origin_region.csv"};
+  std::vector<std::string> value_fields{"PASSENGERS", "SEATS", "DEPARTURES_PERFORMED"};
+  long rows_to_scan{-1}; // Use -1 to scan all rows
+
+  std::string pivot_file_path{
+      "../pax_seats_deps_by_carrier_origin_region_filtered.csv"};
 
   // Defining maps that will allow us to determine which values to include
   // or exclude for various fields:
-  
+
   // std::vector<std::map<std::string, std::vector<std::string>>> include_map{
   //     {{"CARRIER", {"UA", "AA", "DL"}}}};
 
   // The keys within include_map are fields; their corresponding values
   // are vectors of values for that field to include.
-  std::map<std::string, std::vector<std::string>> include_map {
-      {"CARRIER", {"UA", "AA", "DL"}}, {
-        "ORIGIN", {"JFK", "LAX", "ORD", "MIA", "ATL"}}};
+  std::map<std::string, std::vector<std::string>> include_map{
+      {"CARRIER", {"UA", "AA", "DL"}},
+      {"ORIGIN", {"JFK", "LAX", "ORD", "MIA", "ATL"}}};
   std::map<std::string, std::vector<std::string>> exclude_map{
       {"DEST_COUNTRY", {"US"}}};
 
-
-  // Calling scan_to_pivot to calculate average distance values
+  // Calling scan_to_pivot to calculate values
   // by carrier, origin, and region:
-  scan_to_pivot(data_file_path, pivot_val, index_headers, rows_to_scan,
-                pivot_file_path, [&](CSVRow row) -> std::string {
-                  return {row["CARRIER"].get() + "|" + row["ORIGIN"].get() +
-                          "|" + row["REGION"].get()};
-                }, include_map = include_map,
-              exclude_map = exclude_map);
+  scan_to_pivot(
+      data_file_path, value_fields, index_headers, rows_to_scan,
+      pivot_file_path,
+      [&](CSVRow row) -> std::string {
+        return {row["CARRIER"].get() + "|" + row["ORIGIN"].get() + "|" +
+                row["REGION"].get()};
+      },
+      include_map, exclude_map);
+
+// Unfiltered equivalent:
+
+pivot_file_path = "../pax_seats_deps_by_carrier_origin_region.csv";
+std::map<std::string, std::vector<std::string>> unfiltered_map {};
+
+
+  // Calling scan_to_pivot to calculate values
+  // by carrier, origin, and region:
+  scan_to_pivot(
+      data_file_path, value_fields, index_headers, rows_to_scan,
+      pivot_file_path, [&](CSVRow row) -> std::string {
+        return {row["CARRIER"].get() + "|" + row["ORIGIN"].get() + "|" +
+                row["REGION"].get()};
+      },
+       unfiltered_map, unfiltered_map);
+
+
 
   // Updating several variables, then calling scan_to_pivot a
-  // second time to calculate mean distance values by
+  // second time to calculate metrics by
   // carrier and origin only:
 
-  pivot_file_path = "../passengers_by_carrier_origin.csv";
+  pivot_file_path = "../pax_seats_deps_by_carrier_origin_filtered.csv";
   index_headers = "CARRIER|ORIGIN";
 
+  scan_to_pivot(
+      data_file_path, value_fields, index_headers, rows_to_scan,
+      pivot_file_path,
+      [&](CSVRow row) -> std::string {
+        return {row["CARRIER"].get() + "|" + row["ORIGIN"].get()};
+      },include_map, exclude_map);
 
+// Producing an unfiltered equivalent:
+pivot_file_path = "../pax_seats_deps_by_carrier_origin.csv";
 
   scan_to_pivot(
-      data_file_path, pivot_val, index_headers, rows_to_scan, pivot_file_path,
+      data_file_path, value_fields, index_headers, rows_to_scan,
+      pivot_file_path,
       [&](CSVRow row) -> std::string {
         return {row["CARRIER"].get() + "|" + row["ORIGIN"].get()};
       },
-      include_map = include_map,
-    exclude_map = exclude_map);
-
-  // The following code is useful for checking the program's
-  // RAM usage after all other code has been executed.
-  // std::cout << "Press any key to exit.";
-  // char key;
-  // std::cin >> key;
+      unfiltered_map, unfiltered_map);
 
   auto program_end_time = std::chrono::high_resolution_clock::now();
   auto program_run_time =
@@ -129,20 +144,3 @@ Large_Datasets/BTS/T_T100_SEGMENT_ALL_CARRIER_2024.csv"};
   std::cout << "The program finished running after " << program_run_time
             << " seconds.\n";
 }
-
-// Runtime calcs:
-// (These all use the original, non-truncated versions of the
-// datasets.)
-// Original approach (prior to implementing lambda function for
-// developing pivot index): 7.44317 seconds (1st run); 7.32014
-// seconds (second run); 7.24371 seconds (3rd run).
-
-// New approach (that uses lambda function for creating vector-
-// based pivot index: 7.02943 seconds (1st run);
-// 7.06246 seconds (2nd run); 7.09754 seconds (3rd run)
-
-// New approach (that uses strings rather than vectors to store
-// data): 5.91007 seconds; 5.88203 seconds; and 5.97761 seconds.
-
-// New approach (that uses unordered rather than ordered maps):
-// 5.87901 seconds; 5.92743 seconds; and 5.84311 seconds.
--- pivot_compressors.h
+++ pivot_compressors.h
@@ -6,10 +6,12 @@
 #include <functional>
 #include <map>
 #include <string>
+#include <array>
 
 using namespace csv;
 
-void scan_to_pivot(std::string &data_file_path, std::string &pivot_val,
+void scan_to_pivot(std::string &data_file_path, std::vector<
+  std::string>& value_fields,
                    std::string index_headers, long &rows_to_scan,
                    std::string &pivot_file_path,
                    std::function<std::string(CSVRow)> index_gen,
@@ -17,3 +19,6 @@ void scan_to_pivot(std::string &data_file_path, std::string &pivot_val,
                        &include_map,
                   std::map<std::string, std::vector<std::string>>
                        &exclude_map);
+
+// Documentation on this function is available
+// within pivot_compressors.cpp.
